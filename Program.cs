ca3da704b19ea5556b115103a0cd40804ac77771
kierepkae <kierepkaedyta@gmail.com>
2019-12-03 09:27:59 +0000

    clean up code

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace NullableTypes {
     class Program {
         static void Main (string[] args) {
             DateTime? date = null;
+            DateTime? date2 = date.GetValueOrDefault ();
+            DateTime? date3 = date2;
+
+            System.Console.WriteLine (date3.GetValueOrDefault ());
             System.Console.WriteLine (date.GetValueOrDefault ());
             System.Console.WriteLine (date.HasValue);
             // System.Console.WriteLine (date.Value); throws an exception
