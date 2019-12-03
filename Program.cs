6a5e98e0b7f65a9d9a3c820c514b47c9a6d97aaf
kierepkae <kierepkaedyta@gmail.com>
2019-12-03 08:53:09 +0000

    use properties HasValue, Value and GetValueOrDefault method

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ namespace NullableTypes {
     class Program {
         static void Main (string[] args) {
             DateTime? date = null;
+            System.Console.WriteLine (date.GetValueOrDefault ());
+            System.Console.WriteLine (date.HasValue);
+            // System.Console.WriteLine (date.Value); throws an exception
         }
     }
 }
